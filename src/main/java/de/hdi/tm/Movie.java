8f2d43b281d11eeb8badca9112337a0d33c64b10
Sharrow94 <maciej.roszyk@gmai.com>
2022-08-17 23:25:26 +0200

    xx

---
+++ src/main/java/de/hdi/tm/Movie.java
@@ -0,0 +1,47 @@
+package de.hdi.tm;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Example JPA entity.
+ *
+ * To use it, get access to a JPA EntityManager via injection.
+ *
+ * {@code
+ *     @Inject
+ *     EntityManager em;
+ *
+ *     public void doSomething() {
+ *         MyEntity entity1 = new MyEntity();
+ *         entity1.setField("field-1");
+ *         em.persist(entity1);
+ *
+ *         List<MyEntity> entities = em.createQuery("from MyEntity", MyEntity.class).getResultList();
+ *     }
+ * }
+ */
+@Entity
+public class Movie {
+    private Long id;
+    private String titel;
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+}
